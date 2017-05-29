607b7977551fd666768bd7b8737c58913ac21ce4
Jerry Chang <therealjerrycan@gmail.com>
2017-05-28 19:40:35 -0700

    added recursive quicksort

---
+++ recursion/quicksort.hs
@@ -0,0 +1,7 @@
+quicksort :: (Ord a) => [a] -> [a]
+
+quicksort [] = []
+quicksort (x:xs) = 
+    let small = quicksort [a | a <- xs, a <= x]
+        big = quicksort [a | a <- xs, a > x]
+    in small ++ [x] ++ big
